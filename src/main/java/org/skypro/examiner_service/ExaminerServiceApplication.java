1ecf14df221b13a7f7d6dcb78a7f9356811d1293
MariaSizz <buchacik@mail.ru>
2025-07-19 21:33:00 +0300

    Initial commit.

---
+++ src/main/java/org/skypro/examiner_service/ExaminerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.examiner_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminerServiceApplication.class, args);
+	}
+
+}
