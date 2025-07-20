c7aa9dd8c7fe7dbf713f05809a63a77e8e70e2ee
MariaSizz <buchacik@mail.ru>
2025-07-20 21:11:05 +0300

    DZ_all.

---
+++ src/test/java/org/skypro/examiner_service/service/ExaminerServiceImplTest.java
@@ -0,0 +1,43 @@
+package org.skypro.examiner_service.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.skypro.examiner_service.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class ExaminerServiceImplTest {
+    @Autowired
+    private ExaminerServiceImpl examinerService;
+    @Mock
+    private JavaQuestionService questionService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+    @Test
+    void testGetQuestions(){
+        when(questionService.getAll()).thenReturn(List.of(
+                new Question("Q1", "A1"),
+                new Question("Q2", "A2"),
+                new Question("Q3", "A3")
+        ));
+        when(questionService.getRandomQuestion()).thenReturn(
+                new Question("Q1", "A1"),
+                new Question("Q2", "A2"),
+                new Question("Q3", "A3")
+        );
+        final Collection<Question> questions = examinerService.getQuestions(2);
+        assertEquals(2,questions.size());
+    }
+}
